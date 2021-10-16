d5a7705ea8de277b74607f5231614eae47ee751a
seiyaKM <seiya.kumada.0107@gmail.com>
2021-10-17 06:37:05 +0900

    10.2.2 params block

---
+++ sample/10.2.2._params_block.rb
@@ -0,0 +1,14 @@
+def greeting(&block)
+  puts 'good morning'
+  unless block.nil?
+    text = block.call('hello')
+    puts text
+  end
+  puts 'good evening'
+end
+
+greeting do |text|
+  text * 2
+end
+
+greeting
